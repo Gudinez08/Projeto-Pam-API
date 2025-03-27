e98c79e1c00ce867230bdcff3233af8c83800dfc
Leonardo Pessanha <143332467+Gudinez08@users.noreply.github.com>
2025-03-27 10:32:29 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,67 @@
-import 'dart:convert';
-import 'package:api_filmes/drama.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() {
-  runApp(const MaterialApp (title: "App",
-      home: MainApp(),));
+  runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  MainAPP createState() => MainAPP();
-
-}
-
-class MainAPP extends State<MainApp> {
-
-
-
-List<Drama> filmes =List.empty();                                      
-int total = 0;
- Future<void> readJson() async {
-    final String response = await rootBundle.loadString('assets/filmes_drama.json');
-     Iterable data = await json.decode(response);
-    filmes =  List<Drama>.from(data.map((model)=> Drama.fromJson(model)));
-    total = filmes.length;
-    setState(() {
-      filmes;
-      total;
-    });
-}
-
-   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        extendBody: true,
-        body: GridView.count(
-          crossAxisCount: 1,
-          children: List.generate(filmes.length, (index) {
-            return Center(
-                child: FilmeCard(opcao: filmes[index]),
-              );
-          })
-        )
-    
-        @override
-           create class FilmeCard extends StatelessWidget {
-  const FilmeCard({Key key, this.opcao}) : super(key: key);
-  final Drama opcao;
-  
-  Widget build(BuildContext context) {
-    final TextStyle textStyle = Theme.of(context)
-.textTheme.display1;
-        return Card(
-          color: Colors.white,
-          child: Center(
-             child: Column(
-                 mainAxisSize: MainAxisSize.min,
-                 crossAxisAlignment: CrossAxisAlignment.center,
-                   children: <Widget>[
-                Text(opcao.titulo, style: textStyle),
-          ]
-        ),
-      )
+        appBar: AppBar(title: const Text('Filmes de Drama')),
+        body: const FilmeList(),
+      ),
     );
   }
-          }
-      
-        )
-      )  ;
-    
-
 }
 
+class FilmeList extends StatefulWidget {
+  const FilmeList({super.key});
+
+  @override
+  _FilmeListState createState() => _FilmeListState();
+}
+
+class _FilmeListState extends State<FilmeList> {
+  List<dynamic> filmes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadMovies();
+  }
+
+  Future<void> loadMovies() async {
+    final String jsonString = await rootBundle.loadString('assets/filmes_drama.json');
+    final List<dynamic> jsonResponse = json.decode(jsonString);
+    setState(() {
+      filmes = jsonResponse;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return filmes.isEmpty
+        ? const Center(child: CircularProgressIndicator())
+        : ListView.builder(
+          itemCount: filmes.length,
+          itemBuilder: (context, index) {
+            final filme = filmes[index];
+            return Card(
+              margin: const EdgeInsets.all(10),
+              child: ListTile(
+                title: Text(filme["titulo"]),
+                subtitle: Text(
+                  "Ano: ${filme["ano"]} | Descrição: ${filme["descricao"]}  | Diretor: ${filme["diretor"]} ",
+                ),
+              ),
+            );
+          },
+        );
+  }
 }
